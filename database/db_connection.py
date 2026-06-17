6f7e73e815ebab45bd0231d69494948ecc049728
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-06-17 12:05:49 +0300

    mysql connection

---
+++ database/db_connection.py
@@ -0,0 +1,65 @@
+import mysql.connector
+
+class DB_connection:
+    def __init__(self):
+        self.conn = None
+    def get_connection(self):
+        if self.conn is None or not self.conn.is_connected():
+            conn = mysql.connector.connect(
+                host = "localhost",
+                user = "root",
+                password = "1234",
+                database = "Intelligence_db")
+            return conn
+        else:
+            return self.conn
+
+    def create_database(self):
+        conn = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "1234"
+        )
+        cursor = conn.cursor()
+        try:
+            cursor.execute("CREATE DATABASE IF NOT EXISTS Intelligence_db")
+            cursor.execute("USE Intelligence_db")
+            return {"message":"Successfully completed"}
+        except:
+            raise {"message":"error creating the database"}
+
+        finally:
+            cursor.close()
+            conn.close()
+
+    def create_tables(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS agents(
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       name VARCHAR(50) NOT NULL,
+                       specialty VARCHAR(100) NOT NULL,
+                       is_active BOOLEAN DEFAULT TRUE NOT NULL,
+                       completed_missions INT NOT NULL DEFAULT 0 ,
+                       failed_missions INT NOT NULL DEFAULT 0,
+                       agent_rank ENUM("Junior","Senior","Commander")
+                       )""")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS missions(
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       title VARCHAR(100) NOT NULL ,
+                       description TEXT NOT NULL,
+                       location VARCHAR(100) NOT NULL,
+                       difficulty INT NOT NULL,
+                       importance INT NOT NULL,
+                       status ENUM("NEW","ASSIGNED","PROGRESS_IN","COMPLETED","FAILED","CANCELLED") NOT NULL DEFAULT 'NEW',
+                       risk_level VARCHAR(50) NOT NULL,
+                       assigned_agent_id INT DEFAULT NULL
+                       )""")
+        cursor.close()
+        conn.close()
+
+
+
+# db = DB_connection()
+# db.create_database()
+# db.create_tables()
